b6357db4b81be126c77152e9d61d82610c4811ff
Andrei <andrei.massaini@hotmail.com>
2024-03-01 12:07:36 -0300

    solve 2sum, adding big(O)

--- array/twoSumSolution.java
+++ array/twoSumSolution.java
@@ -1,38 +1,33 @@
 /*
  * describe two sum problem: 
  * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * Time complexity: O(n)
+ * Space complexity: O(n)
  */
 package array;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class twoSumSolution {
     public static void main(String[] args) {
         int[] nums = { 2, 7, 11, 15 };
         int target = 9;
-
-        twoSumSolution solution = new twoSumSolution();
-        int[] result = solution.twoSum(nums, target);
-
-        for (int i : result) {
-            System.out.println(i);
-        }
+        System.out.println(Arrays.toString(twoSum(nums, target)));
     }
 
-    public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> prevMap = new HashMap<>();
-
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hash = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            int num = nums[i];
-            int diff = target - num;
-
-            if (prevMap.containsKey(diff)) {
-                return new int[] { prevMap.get(diff), i };
+            int complement = target - nums[i];
+            if (hash.containsKey(complement)) {
+                return new int[] { i, hash.get(complement) };
             }
+            hash.put(nums[i], i);
 
-            prevMap.put(num, i);
         }
-
         return new int[] {};
+
     }
+
 }
